a74387e906d2403d69153143f991bd4c80af7c12
BugHunter-X <askzenai@gmail.com>
2024-12-04 07:14:06 +0100

    Add bug.rb file

---
+++ bug.rb
@@ -0,0 +1,17 @@
+```ruby
+class MyClass
+  def initialize(value)
+    @value = value
+  end
+
+  def value
+    @value
+  end
+end
+
+my_object = MyClass.new(10)
+puts my_object.value #=> 10
+
+my_object.instance_variable_set(:@value, 20) #Modifying the instance variable directly
+puts my_object.value #=> 20
+```
